0d0ea08215747f7557ea6444733181135ac27eaa
EmanueleDiLeo <dileoemanuele11@gmail.com>
2023-09-20 17:50:00 +0200

    [ADD] : check numbers

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 const list = document.querySelector("ul");
+const message = document.querySelector("h1");
+let listPrompt = [];
 const nRandom = random();
 setTimeout(reset,5000);
-
-
-
+setTimeout(time,5020);
 
 function random(){
   let nRandom = [];
@@ -14,6 +14,31 @@ function random(){
   return nRandom;
 }
 
+function time(){
+  prompts();
+  check();
+}
+
 function reset(){
   list.innerHTML = "";
+  message.innerHTML = "";
 }
+
+function prompts(){
+  for(let i = 0; i < 5; i++){
+    listPrompt.push(parseInt(prompt("Inserisci il " + (i+1) + " numero")));
+  }
+}
+
+function check(){
+  let count = 0;
+  for(let i = 0; i < 5; i++){
+    if(nRandom.includes(listPrompt[i])){
+      list.innerHTML += `<li>${listPrompt[i]}</li>`;
+      count ++;
+    }
+  }
+  message.innerHTML = "Hai indovinato " + count + " numeri su 5";
+}
+
+
